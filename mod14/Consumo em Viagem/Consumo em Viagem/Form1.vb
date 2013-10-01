c0cce32f49beac80f48cf07bb599e9111cabcccf
jamorim <jmsamorim@outlook.pt>
2013-10-01 12:22:41 +0100

    exercicio 1 concluido

---
+++ mod14/Consumo em Viagem/Consumo em Viagem/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim num1 As Double
+        Dim num2 As Double
+        Dim num3 As Double
+        Dim res1 As Double
+        Dim res2 As Double
+
+        num1 = TextBox1.Text
+        num2 = TextBox2.Text
+        num3 = TextBox3.Text
+ 
+
+        res1 = num2 * num3 / 100
+        TextBox4.Text = res1
+        res2 = num1 * res1
+        TextBox5.Text = res2
+
+
+    End Sub
+End Class
